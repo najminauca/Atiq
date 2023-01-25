d386ec9536a5a7a09291f2354bd474a5320aff77
Sarah Allisa <sarah.yanuar.nadia.allisa@mni.thm.de>
2023-01-25 10:54:46 +0100

    add typescript for search bar function

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  searchInput: string | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSearch(){
+    console.log("Searching for " + this.searchInput);
   }
 
 }
