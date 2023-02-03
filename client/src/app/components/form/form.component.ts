9727acd96485b481bc688614f1664fe0083cc79d
Sarah Allisa <sarah.yanuar.nadia.allisa@mni.thm.de>
2023-02-03 14:21:09 +0100

    added new forms to add product, can select pictures

--- client/src/app/components/form/form.component.ts
+++ client/src/app/components/form/form.component.ts
@@ -6,14 +6,30 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  selectedPicture: string | ArrayBuffer;
 
-  constructor() { }
+  constructor() {
+    this.selectedPicture = ""
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
 
+  }
+
+  selectPicture(event: any) {
+    /*
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      this.selectedPicture = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+    */
   }
 
 }
