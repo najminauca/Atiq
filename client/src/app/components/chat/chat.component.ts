03ca7739e94f8e43f525772eed65461dc0e4547b
Sarah Allisa <sarah.yanuar.nadia.allisa@mni.thm.de>
2023-02-08 10:12:56 +0100

    finished chat page

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  messages : string[] = [];
+  newMessage: string;
+  users = ['Hicham', 'Najmi', 'Yohanes', 'Sarah'];
+  selectedUser: string;
+  selectedUserButton = false;
 
-  constructor() { }
+  constructor() {
+    this.newMessage = "";
+    this.selectedUser = this.users[0];
+  }
 
   ngOnInit(): void {
   }
 
+  sendMessage() {
+    this.messages.push(this.newMessage);
+    this.newMessage = '';
+  }
+
+  switchChat(user: string) {
+    this.selectedUser = user;
+  }
+
+  toggleHighlight() {
+    this.selectedUserButton = !this.selectedUserButton;
+  }
 }
